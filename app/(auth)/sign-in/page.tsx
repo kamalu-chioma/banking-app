8dc7fd4a021b712b489c2fed0befd9b9042eb2c2
Chioma Kamalu <kamaluchioma062@gmail.com>
2024-06-20 11:40:44 -0400

    implement initial file and folder

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>
+            Sign In 
+            {/* Your component content goes here */}
+        </div>
+    );
+};
+
+export default SignIn;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            Home
+            {/* Your component content goes here */}
+        </div>
+    );
+};
+
+export default Home;
